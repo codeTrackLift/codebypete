e549cc03f4e0fd6273d635337e87417091a294c0
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-02-01 15:48:35 -0500

    opaque welcome background

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var controllerFadeIn1 = new ScrollMagic.Controller();
 var fadeIn1 = new ScrollMagic.Scene({
     triggerElement: '.fadeIn1',
-    offset: 100,
-    duration: 700
 })
 .setClassToggle('.fadeIn1', 'show')
 .addTo(controllerFadeIn1);
@@ -10,8 +8,6 @@ var fadeIn1 = new ScrollMagic.Scene({
 var controllerFadeIn2 = new ScrollMagic.Controller();
 var fadeIn2 = new ScrollMagic.Scene({
     triggerElement: '.fadeIn2',
-    offset: -200,
-    duration: 700
 })
 .setClassToggle('.fadeIn2', 'show')
 .addTo(controllerFadeIn2);
@@ -19,8 +15,6 @@ var fadeIn2 = new ScrollMagic.Scene({
 var controllerFadeIn3 = new ScrollMagic.Controller();
 var fadeIn3 = new ScrollMagic.Scene({
     triggerElement: '.fadeIn3',
-    offset: -350,
-    duration: 700
 })
 .setClassToggle('.fadeIn3', 'show')
 .addTo(controllerFadeIn1);
