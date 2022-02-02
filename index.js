559d180778a0a3fd898939a04d771a56a8e28be1
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-02-02 09:36:16 -0500

    better welcome animations using keyframes

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var controllerFadeIn1 = new ScrollMagic.Controller();
-var fadeIn1 = new ScrollMagic.Scene({
-    triggerElement: '.fadeIn1',
-})
-.setClassToggle('.fadeIn1', 'show')
-.addTo(controllerFadeIn1);
+// var controllerFadeIn1 = new ScrollMagic.Controller();
+// var fadeIn1 = new ScrollMagic.Scene({
+//     triggerElement: '.fadeIn1',
+// })
+// .setClassToggle('.fadeIn1', 'show')
+// .addTo(controllerFadeIn1);
 
-var controllerFadeIn2 = new ScrollMagic.Controller();
-var fadeIn2 = new ScrollMagic.Scene({
-    triggerElement: '.fadeIn2',
-})
-.setClassToggle('.fadeIn2', 'show')
-.addTo(controllerFadeIn2);
+// var controllerFadeIn2 = new ScrollMagic.Controller();
+// var fadeIn2 = new ScrollMagic.Scene({
+//     triggerElement: '.fadeIn2',
+// })
+// .setClassToggle('.fadeIn2', 'show')
+// .addTo(controllerFadeIn2);
 
-var controllerFadeIn3 = new ScrollMagic.Controller();
-var fadeIn3 = new ScrollMagic.Scene({
-    triggerElement: '.fadeIn3',
-    offset: -300
-})
-.setClassToggle('.fadeIn3', 'show')
-.addTo(controllerFadeIn3);
+// var controllerFadeIn3 = new ScrollMagic.Controller();
+// var fadeIn3 = new ScrollMagic.Scene({
+//     triggerElement: '.fadeIn3',
+//     offset: -300
+// })
+// .setClassToggle('.fadeIn3', 'show')
+// .addTo(controllerFadeIn3);
 
-var controllerFadeIn4 = new ScrollMagic.Controller();
-var fadeIn4 = new ScrollMagic.Scene({
-    triggerElement: '.fadeIn4',
-    offset: -200
-})
-.setClassToggle('.fadeIn4', 'show')
-.addTo(controllerFadeIn4);
+// var controllerFadeIn4 = new ScrollMagic.Controller();
+// var fadeIn4 = new ScrollMagic.Scene({
+//     triggerElement: '.fadeIn4',
+//     offset: -200
+// })
+// .setClassToggle('.fadeIn4', 'show')
+// .addTo(controllerFadeIn4);
