6fab55fc44c616d872d504dae45697f98fdc1015
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-03-01 14:38:25 -0500

    proj repo btns blog3 video

--- js/blog.js
+++ js/blog.js
@@ -1,23 +1,32 @@
+// Blog div .accordion-collapse 
 const blog3 = document.getElementById('blog3');
 const blog2 = document.getElementById('blog2');
 const blog1 = document.getElementById('blog1');
 const blog0 = document.getElementById('blog0');
+
+// Blog buttons
 const blog3Button = document.getElementById('blog3Button');
 const blog2Button = document.getElementById('blog2Button');
 const blog1Button = document.getElementById('blog1Button');
 const blog0Button = document.getElementById('blog0Button');
 
-// Set to true if starting open
+// Blog starting values, set to true if starting open
 var blog3OpenStatus = false; 
 var blog2OpenStatus = false;
 var blog1OpenStatus = false;
 var blog0OpenStatus = false;
 
+// Blog videos
+const blog3Video = document.getElementById('blog3Video');
+const blog2Video = document.getElementById('blog2Video');
+
+// Blog button functions
 function blog3Open() {
   if (!blog3OpenStatus) {
     blog3OpenStatus = !blog3OpenStatus;
     blog3.classList.add('show');
     blog3Button.classList.remove('collapsed');
+    blog3Video.src = 'https://youtube.com/embed/iOKK8Q2unxE?&autoplay=1&mute=1';
     return
   }
   blog3OpenStatus = !blog3OpenStatus;
@@ -30,11 +39,13 @@ function blog2Open() {
     blog2OpenStatus = !blog2OpenStatus;
     blog2.classList.add('show');
     blog2Button.classList.remove('collapsed');
+    blog2Video.src = 'https://youtube.com/embed/f0mTQx5ATQI?&autoplay=1&mute=1';
     return
   }
   blog2OpenStatus = !blog2OpenStatus;
   blog2.classList.remove('show');
   blog2Button.classList.add('collapsed');
+  blog2Video.src = 'https://youtube.com/embed/f0mTQx5ATQI';
 }
 
 function blog1Open() {
