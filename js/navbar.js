003c2634bf46bfc3e52b92ade1e5e918dde58fc0
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-03-16 20:12:28 -0400

    navbar logo webkit dropshadow

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ const navbar = document.getElementById('navbar');
 const myNavbar = `
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <img id='pharm2CodeLogo' src='../pics/pharma2code_icon.gif' style="width:2.5rem;">
+            <img id='pharm2CodeLogo' src='../pics/pharma2code_icon.gif'>
         </button>
         <h3 id='navbarCBP' class="d-block d-sm-none" style="font-style:italic;letter-spacing:2vw;"><span style="color:white;">code</span><span style="color:#00857c;">By</span><span style="color:lime;">Pete</span></h3>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
