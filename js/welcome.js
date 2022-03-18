e621ee8e1bcaaf926a973f0672572ac5bade8610
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-03-18 17:58:48 -0400

    github contributions add

--- js/welcome.js
+++ js/welcome.js
@@ -56,6 +56,14 @@ const myWelcome = `
             </div>
         </div>
 
+
+
+    </div>
+
+    <div id='githubContributions' class="container-fluid text-center">
+        <a href="https://github.com/codeTrackLift" target="_blank">
+            <img src="../pics/about/githubContributions.png" alt="GitHub contribution">
+        </a>
     </div>
 
     </section>
