5169bc82b5c99e5865494f4295e4eb66c511e2ab
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-03-23 13:21:28 -0400

    em btn scrollm offset

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 var emailButtonController = new ScrollMagic.Controller();
 var emailButton = new ScrollMagic.Scene({
         triggerElement: '#emailButton',
-        offset: -100
+        offset: -300
     })
     .setClassToggle('#emailButton', 'show')
     .addTo(emailButtonController);
