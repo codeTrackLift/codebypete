8c0f94cedbf1850483f2f79baf384c3789e3df18
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-13 09:00:14 -0400

    react projs

--- js/projects.js
+++ js/projects.js
@@ -1,166 +1,189 @@
+var lenovaincDemoController = new ScrollMagic.Controller();
+var lenovaincDemo = new ScrollMagic.Scene({
+        triggerElement: '#lenovaincDemo',
+        offset: -300
+    })
+    .setClassToggle('#lenovaincDemo', 'show')
+    .addTo(lenovaincDemoController);
+
+var lmnoPhotoDemoController = new ScrollMagic.Controller();
+var lmnoPhotoDemo = new ScrollMagic.Scene({
+        triggerElement: '#lmnoPhotoDemo',
+        offset: -300
+    })
+    .setClassToggle('#lmnoPhotoDemo', 'show')
+    .addTo(lmnoPhotoDemoController);
+
 var busTrackerDemoController = new ScrollMagic.Controller();
 var busTrackerDemo = new ScrollMagic.Scene({
-    triggerElement: '#busTrackerDemo',
-    offset: -300
-})
-.setClassToggle('#busTrackerDemo', 'show')
-.addTo(busTrackerDemoController);
+        triggerElement: '#busTrackerDemo',
+        offset: -300
+    })
+    .setClassToggle('#busTrackerDemo', 'show')
+    .addTo(busTrackerDemoController);
 
 var busTrackerRepoController = new ScrollMagic.Controller();
 var busTrackerRepo = new ScrollMagic.Scene({
         triggerElement: '#busTrackerRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#busTrackerRepo', 'show')
     .addTo(busTrackerRepoController);
 
 var animatedHeadDemoController = new ScrollMagic.Controller();
 var animatedHeadDemo = new ScrollMagic.Scene({
-    triggerElement: '#animatedHeadDemo',
-    offset: -300
-})
-.setClassToggle('#animatedHeadDemo', 'show')
-.addTo(animatedHeadDemoController);
+        triggerElement: '#animatedHeadDemo',
+        offset: -300
+    })
+    .setClassToggle('#animatedHeadDemo', 'show')
+    .addTo(animatedHeadDemoController);
 
 var animatedHeadRepoController = new ScrollMagic.Controller();
 var animatedHeadRepo = new ScrollMagic.Scene({
         triggerElement: '#animatedHeadRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#animatedHeadRepo', 'show')
     .addTo(animatedHeadRepoController);
 
 var pacManFactoryDemoController = new ScrollMagic.Controller();
 var pacManFactoryDemo = new ScrollMagic.Scene({
-    triggerElement: '#pacManFactoryDemo',
-    offset: -300
-})
-.setClassToggle('#pacManFactoryDemo', 'show')
-.addTo(pacManFactoryDemoController);
+        triggerElement: '#pacManFactoryDemo',
+        offset: -300
+    })
+    .setClassToggle('#pacManFactoryDemo', 'show')
+    .addTo(pacManFactoryDemoController);
 
 var pacManFactoryRepoController = new ScrollMagic.Controller();
 var pacManFactoryRepo = new ScrollMagic.Scene({
         triggerElement: '#pacManFactoryRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#pacManFactoryRepo', 'show')
     .addTo(pacManFactoryRepoController);
 
 var pacManMiniGameDemoController = new ScrollMagic.Controller();
 var pacManMiniGameDemo = new ScrollMagic.Scene({
-    triggerElement: '#pacManMiniGameDemo',
-    offset: -300
-})
-.setClassToggle('#pacManMiniGameDemo', 'show')
-.addTo(pacManMiniGameDemoController);
+        triggerElement: '#pacManMiniGameDemo',
+        offset: -300
+    })
+    .setClassToggle('#pacManMiniGameDemo', 'show')
+    .addTo(pacManMiniGameDemoController);
 
 var pacManMiniGameRepoController = new ScrollMagic.Controller();
 var pacManMiniGameRepo = new ScrollMagic.Scene({
         triggerElement: '#pacManMiniGameRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#pacManMiniGameRepo', 'show')
     .addTo(pacManMiniGameRepoController);
 
-var lenovaincDemoController = new ScrollMagic.Controller();
-var lenovaincDemo = new ScrollMagic.Scene({
-    triggerElement: '#lenovaincDemo',
-    offset: -300
-})
-.setClassToggle('#lenovaincDemo', 'show')
-.addTo(lenovaincDemoController);
+var pomodoroClockDemoController = new ScrollMagic.Controller();
+var pomodoroClockDemo = new ScrollMagic.Scene({
+        triggerElement: '#pomodoroClockDemo',
+        offset: -300
+    })
+    .setClassToggle('#pomodoroClockDemo', 'show')
+    .addTo(pomodoroClockDemoController);
 
-var lmnoPhotoDemoController = new ScrollMagic.Controller();
-var lmnoPhotoDemo = new ScrollMagic.Scene({
-    triggerElement: '#lmnoPhotoDemo',
-    offset: -300
-})
-.setClassToggle('#lmnoPhotoDemo', 'show')
-.addTo(lmnoPhotoDemoController);
+var pomodoroClockRepoController = new ScrollMagic.Controller();
+var pomodoroClockRepo = new ScrollMagic.Scene({
+        triggerElement: '#pomodoroClockRepo',
+        offset: -250
+    })
+    .setClassToggle('#pomodoroClockRepo', 'show')
+    .addTo(pomodoroClockRepoController);
 
-// var lmnoPhotoRepoController = new ScrollMagic.Controller();
-// var lmnoPhotoRepo = new ScrollMagic.Scene({
-//         triggerElement: '#lmnoPhotoRepo',
-//         offset: -300
-//     })
-//     .setClassToggle('#lmnoPhotoRepo', 'show')
-//     .addTo(lmnoPhotoRepoController);
+var markdownPreviewerDemoController = new ScrollMagic.Controller();
+var markdownPreviewerDemo = new ScrollMagic.Scene({
+        triggerElement: '#markdownPreviewerDemo',
+        offset: -300
+    })
+    .setClassToggle('#markdownPreviewerDemo', 'show')
+    .addTo(markdownPreviewerDemoController);
+
+var markdownPreviewerRepoController = new ScrollMagic.Controller();
+var markdownPreviewerRepo = new ScrollMagic.Scene({
+        triggerElement: '#markdownPreviewerRepo',
+        offset: -250
+    })
+    .setClassToggle('#markdownPreviewerRepo', 'show')
+    .addTo(markdownPreviewerRepoController);
 
 var jsCalcDemoController = new ScrollMagic.Controller();
 var jsCalcDemo = new ScrollMagic.Scene({
-    triggerElement: '#jsCalcDemo',
-    offset: -300
-})
-.setClassToggle('#jsCalcDemo', 'show')
-.addTo(jsCalcDemoController);
+        triggerElement: '#jsCalcDemo',
+        offset: -300
+    })
+    .setClassToggle('#jsCalcDemo', 'show')
+    .addTo(jsCalcDemoController);
 
 var jsCalcRepoController = new ScrollMagic.Controller();
 var jsCalcRepo = new ScrollMagic.Scene({
         triggerElement: '#jsCalcRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#jsCalcRepo', 'show')
     .addTo(jsCalcRepoController);
 
 var etchASketchDemoController = new ScrollMagic.Controller();
 var etchASketchDemo = new ScrollMagic.Scene({
-    triggerElement: '#etchASketchDemo',
-    offset: -300
-})
-.setClassToggle('#etchASketchDemo', 'show')
-.addTo(etchASketchDemoController);
+        triggerElement: '#etchASketchDemo',
+        offset: -300
+    })
+    .setClassToggle('#etchASketchDemo', 'show')
+    .addTo(etchASketchDemoController);
 
 var etchASketchRepoController = new ScrollMagic.Controller();
 var etchASketchRepo = new ScrollMagic.Scene({
         triggerElement: '#etchASketchRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#etchASketchRepo', 'show')
     .addTo(etchASketchRepoController);
 
 var rockPaperScissorsDemoController = new ScrollMagic.Controller();
 var rockPaperScissorsDemo = new ScrollMagic.Scene({
-    triggerElement: '#rockPaperScissorsDemo',
-    offset: -300
-})
-.setClassToggle('#rockPaperScissorsDemo', 'show')
-.addTo(rockPaperScissorsDemoController);
+        triggerElement: '#rockPaperScissorsDemo',
+        offset: -300
+    })
+    .setClassToggle('#rockPaperScissorsDemo', 'show')
+    .addTo(rockPaperScissorsDemoController);
 
 var rockPaperScissorsRepoController = new ScrollMagic.Controller();
 var rockPaperScissorsRepo = new ScrollMagic.Scene({
         triggerElement: '#rockPaperScissorsRepo',
-        offset: -300
+        offset: -250
     })
     .setClassToggle('#rockPaperScissorsRepo', 'show')
     .addTo(rockPaperScissorsRepoController);
 
 var odinLandingpageDemoController = new ScrollMagic.Controller();
 var odinLandingpageDemo = new ScrollMagic.Scene({
-    triggerElement: '#odinLandingpageDemo',
-    offset: -350
-})
-.setClassToggle('#odinLandingpageDemo', 'show')
-.addTo(odinLandingpageDemoController);
+        triggerElement: '#odinLandingpageDemo',
+        offset: -300
+    })
+    .setClassToggle('#odinLandingpageDemo', 'show')
+    .addTo(odinLandingpageDemoController);
 
 var odinLandingpageRepoController = new ScrollMagic.Controller();
 var odinLandingpageRepo = new ScrollMagic.Scene({
         triggerElement: '#odinLandingpageRepo',
-        offset: -350
+        offset: -250
     })
     .setClassToggle('#odinLandingpageRepo', 'show')
     .addTo(odinLandingpageRepoController);
-    
-    var buttonBlogController = new ScrollMagic.Controller();
-    var buttonBlog = new ScrollMagic.Scene({
+
+var buttonBlogController = new ScrollMagic.Controller();
+var buttonBlog = new ScrollMagic.Scene({
         triggerElement: '#buttonBlog',
         offset: -350
     })
     .setClassToggle('#buttonBlog', 'show')
     .addTo(buttonBlogController);
-    
-    
-    var buttonContactController = new ScrollMagic.Controller();
-    var buttonContact = new ScrollMagic.Scene({
+
+var buttonContactController = new ScrollMagic.Controller();
+var buttonContact = new ScrollMagic.Scene({
         triggerElement: '#buttonContact',
         offset: -350
     })
