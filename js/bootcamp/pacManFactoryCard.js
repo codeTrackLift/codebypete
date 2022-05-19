8fed45a7f2e8337363ccce49976d509ef1c39daf
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-19 18:04:55 -0400

    aquapac jpg

--- js/bootcamp/pacManFactoryCard.js
+++ js/bootcamp/pacManFactoryCard.js
@@ -1,6 +1,6 @@
 const pacManFactoryCard = `
     <div id='pacManFactoryCard' class="card col-sm-4" style="width: 18rem;">
-        <img src="../pics/pacman/aquaPacMan.png" class="card-img-top mx-auto pt-3 img-fluid" alt="Aqua-PacMan animated gif." style="width:10rem;">
+        <img src="../pics/pacman/aquaPacMan.jpg" class="card-img-top mx-auto pt-3 img-fluid" alt="Aqua-PacMan animated gif." style="width:10rem;">
         <div class="card-body">
             <h5 class="card-title">PacMan Factory</h5>
             <p class="card-text">This <a href="https://xpro.mit.edu/" target="_blank">MIT xPRO</a> <span> Bootcamp Project</span> demonstrates use of array methods and DOM manipulation.</p>
