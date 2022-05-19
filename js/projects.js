85b53bd76765bfc19fe4bbfd817f147599bada4c
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-19 13:30:26 -0400

    projlist js reorg

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,4 @@
+// Projects sections
 const projectsContainer = document.getElementById('projectsContainer');
 const categoryButton = document.getElementById('categoryButton');
 const newestButton = document.getElementById('newestButton');
@@ -43,98 +44,7 @@ const oldestSection = `
     </div>
 `;
 
-const injectLoading = () => {
-    projectsContainer.innerHTML = `
-        <div class="text-center my-5 text-silver"><span class="spinner-border" role="status"></span><p>Loading...</p></div>
-    `;
-}
-
-const clearSortButtons = () => {
-    categoryButton.classList.remove('active');
-    newestButton.classList.remove('active');
-    oldestButton.classList.remove('active');
-}
-
-const onclickSortButtons = () => {
-    categoryButton.setAttribute('onclick', `
-        injectLoading();
-        changeCategoryButton();
-        setTimeout(injectProjectsCategory,500);
-    `);
-    newestButton.setAttribute('onclick', `
-        injectLoading();
-        changeNewestButton();
-        setTimeout(injectProjectsNewest,500);
-    `);
-    oldestButton.setAttribute('onclick', `
-        injectLoading();
-        changeOldestButton();
-        setTimeout(injectProjectsOldest,500);
-    `);
-}
-
-const changeCategoryButton = () => {
-    clearSortButtons();
-    onclickSortButtons();
-    categoryButton.setAttribute('onclick', '');
-    categoryButton.classList.add('active');
-}
-
-const changeNewestButton = () => {
-    clearSortButtons();
-    onclickSortButtons();
-    newestButton.classList.add('active');
-    newestButton.setAttribute('onclick', '');
-}
-
-const changeOldestButton = () => {
-    clearSortButtons();
-    onclickSortButtons();
-    oldestButton.classList.add('active');
-    oldestButton.setAttribute('onclick', '');
-}
-
-const injectProjectsCategory = () => {
-    projectsContainer.innerHTML = businessProjectsSection;
-    const businessProjects = document.getElementById('businessProjects');
-    injectLenovaIncCard(businessProjects);
-    injectLmnoPhotoCard(businessProjects);
-    
-    projectsContainer.innerHTML += bootcampProjectsSection;
-    const bootcampProjects = document.getElementById('bootcampProjects');
-    injectxChangeCard(bootcampProjects);
-    injectTodoCard(bootcampProjects);
-    injectBusTrackerCard(bootcampProjects);
-    injectAnimatedHeadCard(bootcampProjects);
-    injectPacManFactoryCard(bootcampProjects);
-    injectPacManMiniGameCard(bootcampProjects);
-    
-    projectsContainer.innerHTML += selfTaughtProjectsSection;
-    const selfTaughtProjects = document.getElementById('selfTaughtProjects');    
-    injectPomodoroCard(selfTaughtProjects);
-    injectMarkdownPreviewerCard(selfTaughtProjects);
-    injectJsCalcCard(selfTaughtProjects);
-    injectEtchASketchCard(selfTaughtProjects);
-    injectRockPaperScissorsCard(selfTaughtProjects);
-    injectOdinLandingpageCard(selfTaughtProjects);
-}
-
-const injectProjectsNewest = () => {
-    projectsContainer.innerHTML = newestSection;
-    
-    for( let i = 0; i < projectsList.length; i++ ) {
-        projectsList[i](newestProjects)
-    }
-}
-
-const injectProjectsOldest = () => {
-    projectsContainer.innerHTML = oldestSection;
-    
-    for( let i = projectsList.length; i > 0; i-- ) {
-        projectsList[i - 1](oldestProjects)
-    }
-}
-
+// Bottom nav buttons
 const buttonBlogController = new ScrollMagic.Controller();
 const buttonBlog = new ScrollMagic.Scene({
         triggerElement: '#buttonBlog',
--- js/projectsList.js
+++ js/projectsList.js
@@ -1,6 +1,5 @@
+// List projects newest to oldest
 const projectsList = [];
-
-// List newest to oldest
 projectsList.push(injectxChangeCard);
 projectsList.push(injectTodoCard);
 projectsList.push(injectPomodoroCard);
@@ -15,3 +14,98 @@ projectsList.push(injectJsCalcCard);
 projectsList.push(injectEtchASketchCard);
 projectsList.push(injectRockPaperScissorsCard);
 projectsList.push(injectOdinLandingpageCard);
+
+const injectLoading = () => {
+    projectsContainer.innerHTML = `
+        <div class="text-center my-5 text-silver"><span class="spinner-border" role="status"></span><p>Loading...</p></div>
+    `;
+}
+
+// Remove active class from all sort buttons
+const clearSortButtons = () => {
+    categoryButton.classList.remove('active');
+    newestButton.classList.remove('active');
+    oldestButton.classList.remove('active');
+}
+
+// Add onclick to add sort buttons
+const onclickSortButtons = () => {
+    categoryButton.setAttribute('onclick', `
+        injectLoading();
+        changeCategoryButton();
+        setTimeout(injectProjectsCategory,500);
+    `);
+    newestButton.setAttribute('onclick', `
+        injectLoading();
+        changeNewestButton();
+        setTimeout(injectProjectsNewest,500);
+    `);
+    oldestButton.setAttribute('onclick', `
+        injectLoading();
+        changeOldestButton();
+        setTimeout(injectProjectsOldest,500);
+    `);
+}
+
+const changeCategoryButton = () => {
+    clearSortButtons();
+    onclickSortButtons();
+    categoryButton.setAttribute('onclick', '');
+    categoryButton.classList.add('active');
+}
+
+const changeNewestButton = () => {
+    clearSortButtons();
+    onclickSortButtons();
+    newestButton.classList.add('active');
+    newestButton.setAttribute('onclick', '');
+}
+
+const changeOldestButton = () => {
+    clearSortButtons();
+    onclickSortButtons();
+    oldestButton.classList.add('active');
+    oldestButton.setAttribute('onclick', '');
+}
+
+// Project sort injection functions
+const injectProjectsCategory = () => {
+    projectsContainer.innerHTML = businessProjectsSection;
+    const businessProjects = document.getElementById('businessProjects');
+    injectLenovaIncCard(businessProjects);
+    injectLmnoPhotoCard(businessProjects);
+    
+    projectsContainer.innerHTML += bootcampProjectsSection;
+    const bootcampProjects = document.getElementById('bootcampProjects');
+    injectxChangeCard(bootcampProjects);
+    injectTodoCard(bootcampProjects);
+    injectBusTrackerCard(bootcampProjects);
+    injectAnimatedHeadCard(bootcampProjects);
+    injectPacManFactoryCard(bootcampProjects);
+    injectPacManMiniGameCard(bootcampProjects);
+    
+    projectsContainer.innerHTML += selfTaughtProjectsSection;
+    const selfTaughtProjects = document.getElementById('selfTaughtProjects');    
+    injectPomodoroCard(selfTaughtProjects);
+    injectMarkdownPreviewerCard(selfTaughtProjects);
+    injectJsCalcCard(selfTaughtProjects);
+    injectEtchASketchCard(selfTaughtProjects);
+    injectRockPaperScissorsCard(selfTaughtProjects);
+    injectOdinLandingpageCard(selfTaughtProjects);
+}
+
+const injectProjectsNewest = () => {
+    projectsContainer.innerHTML = newestSection;
+    
+    for( let i = 0; i < projectsList.length; i++ ) {
+        projectsList[i](newestProjects)
+    }
+}
+
+const injectProjectsOldest = () => {
+    projectsContainer.innerHTML = oldestSection;
+    
+    for( let i = projectsList.length; i > 0; i-- ) {
+        projectsList[i - 1](oldestProjects)
+    }
+}
