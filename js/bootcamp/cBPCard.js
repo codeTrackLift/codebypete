9959844d3ac9197899059660c8401f52bde2789d
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-19 15:10:50 -0400

    cbp card wordsm

--- js/bootcamp/cBPCard.js
+++ js/bootcamp/cBPCard.js
@@ -2,7 +2,7 @@ const cBPCard = `
     <div id='cBPCard' class="card col-sm-4" style="width: 18rem;">
         <img src="../pics/projects/giphycBP.gif" class="card-img-top m-auto" alt="codeByPete welcome animation">
         <div class="card-body">
-            <h5 class="card-title">codeByPete.com</h5>
+            <h5 class="card-title">codebypete.com</h5>
             <p class="card-text">Developed using <span class='text-white'>vanilla HTML</span>, <span class='text-white'>CSS</span>, and <span class='text-white'>JavaScript</span> with <span class='text-white'>Bootstrap</span> and <span class='text-white'>ScrollMagic</span> libraries.</p>
             <p class="card-text"><span class='text-white'>Mobile first</span> and <span class='text-white'>responsive design</span>. Custom JavaScript functionality and CSS animations, see <a href="../pages/blog.html#blog4Button">blog[4]</a>.</p>
             <div class="btnContainer">
