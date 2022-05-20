0ab13d139cda64be721925d521ba14bb4d9edefe
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-20 19:00:11 -0400

    socials

--- js/footer.js
+++ js/footer.js
@@ -1,24 +1,19 @@
 const currentYear = new Date().getFullYear();
 const footer = document.getElementById('footer');
+
 const myFooter = `
-    <p>Copyright ©
+    <p class='text-silver'>Copyright ©
         ${currentYear}
         <img class='d-none d-sm-inline' src="../pics/pharma2code_icon.gif" style='height: 2rem;' alt="My 'Pharma 2 Code' Logo.">
         <span style="color:white;">code</span><span style="color:#00857c;">By</span><span style="color:lime;">Pete</span>
     </p>
-    <a href="https://github.com/codeTrackLift/" target="_blank">
-        <img class="socialLogo" src="../pics/contact/GitHub-Mark-Light-64px.png" alt="GitHub Icon">
-    </a>
-    <a href="https://twitter.com/codeTrackLift" target="_blank">
-        <img class="socialLogo" src="../pics/contact/Twitter_social_icons-circle-white.png" alt="Twitter Icon">
-    </a>
-    <a href="https://www.linkedin.com/in/codebypete/" target="_blank">
-        <img class="socialLogo" src="../pics/contact/linkedin_white.png" alt="LinkedIn Icon">
-    </a>`;
+
+`;
 
 const injectMyFooter = () => footer.innerHTML = myFooter;
 
 const footerBackground = () => {
+    socials.style.backgroundColor = 'rgb(50,50,50)';
     footer.style.backgroundColor = 'rgb(50,50,50)';
     footer.style.height = '3rem';
     footer.style.marginTop = '0';
---
+++ js/socials.js
@@ -0,0 +1,24 @@
+const socials = document.getElementById('socials');
+
+const socialLogos = `
+    <div id='socials'>
+        <a href="https://github.com/codeTrackLift/" target="_blank" rel='noreferrer'>
+            <img src='../pics/contact/GitHub-Mark-Light-64px.png' id='gitHubLogo' class='socialLogo' alt="GitHub Icon">
+        </a>
+        <a href="https://twitter.com/codetracklift" target="_blank" rel='noreferrer'>
+            <img src='../pics/contact/Twitter_social_icons-circle-white.png' id='twitterLogo' class='socialLogo' alt="Twitter Icon">
+        </a>
+        <a href="https://www.linkedin.com/in/codebypete/" 
+            target="_blank" rel='noreferrer'>
+            <img src='../pics/contact/linkedin_white.png' id='linkedInLogo' class='socialLogo' alt="LinkedIn Icon">
+        </a>
+            <a href="https://www.youtube.com/channel/UCVDOFoM5NXYrPoC02lbNJ-Q" target="_blank" rel='noreferrer'>
+            <img src='../pics/contact/yt_logo.gif' id='youTubeLogo' class='socialLogo' alt="YouTube Icon">
+        </a>
+    </div>
+`;
+
+const injectSocials = () => {
+    socials.innerHTML = socialLogos;
+    socials.style.paddingBlock = '1rem';
+}
