1302b28ed0ab271b30a9c3836f5b2403afb87ffd
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-20 20:32:40 -0400

    social order

--- js/socials.js
+++ js/socials.js
@@ -5,9 +5,6 @@ const socialLogos = `
         <a href="https://github.com/codeTrackLift/" target="_blank" rel='noreferrer'>
             <img src='../pics/contact/GitHub-Mark-Light-64px.png' id='gitHubLogo' class='socialLogo' alt="GitHub Icon">
         </a>
-        <a href="https://twitter.com/codetracklift" target="_blank" rel='noreferrer'>
-            <img src='../pics/contact/Twitter_social_icons-circle-white.png' id='twitterLogo' class='socialLogo' alt="Twitter Icon">
-        </a>
         <a href="https://www.linkedin.com/in/codebypete/" 
             target="_blank" rel='noreferrer'>
             <img src='../pics/contact/linkedin_white.png' id='linkedInLogo' class='socialLogo' alt="LinkedIn Icon">
@@ -15,10 +12,14 @@ const socialLogos = `
             <a href="https://www.youtube.com/channel/UCVDOFoM5NXYrPoC02lbNJ-Q" target="_blank" rel='noreferrer'>
             <img src='../pics/contact/yt_logo.gif' id='youTubeLogo' class='socialLogo' alt="YouTube Icon">
         </a>
+        <a href="https://twitter.com/codetracklift" target="_blank" rel='noreferrer'>
+            <img src='../pics/contact/Twitter_social_icons-circle-white.png' id='twitterLogo' class='socialLogo' alt="Twitter Icon">
+        </a>
     </div>
 `;
 
 const injectSocials = () => {
     socials.innerHTML = socialLogos;
-    document.getElementById('socialsContainer').style.paddingBlock = '1rem';
+    socialsContainer = document.getElementById('socialsContainer')
+    socialsContainer.style.paddingBlock = '1rem';
 }
