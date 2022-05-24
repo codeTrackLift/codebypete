3e930d31400f4b40445283d9bfc35ee2fb45f126
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-24 18:27:28 -0400

    wordsm

--- js/selfTaught/xChMernCard.js
+++ js/selfTaught/xChMernCard.js
@@ -3,7 +3,7 @@ const xChangeMernCard = `
         <img src="../pics/xchange/xchmern.jpg" class="card-img-top m-auto px-5 pt-2 img-fluid" alt="MIT xChangeMern animated gif with stock ticker.">
         <div class="card-body">
             <h5 class="card-title">Full Stack Bank App</h5>
-            <p class="card-text"><a href='#xChangeCard'>BadBank</a> was upgraded into a full stack MERN app using <span className='text-white'>MongoDB</span>, <span className='text-white'>Mongoose</span>, <span className='text-white'>Express</span>, <span className='text-white'>React</span>, and <span className='text-white'>Redux</span>.</p>
+            <p class="card-text"><a href='#xChangeCard'>Bad Bank</a> was upgraded into a full stack MERN app using <span className='text-white'>MongoDB</span>, <span className='text-white'>Mongoose</span>, <span className='text-white'>Express</span>, <span className='text-white'>React</span>, and <span className='text-white'>Redux</span>.</p>
             <p class="card-text">The demo is deployed on <span className='text-white'>Heroku</span>, please give it a min to spin up.</p>
             <div class="btnContainer">
                 <a id='xChangeMernDemo' href="https://xch-mern.herokuapp.com/" class="btn btn-secondary" target="_blank">Try Demo</a>
