300485961f3aaf947a6c617a1abfdc4a00921e8c
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-06-24 18:38:02 -0400

    pStyles

--- js/blogs/blog5.js
+++ js/blogs/blog5.js
@@ -1,10 +1,14 @@
+const blog5pStyle = `
+    style='margin: 0.5rem auto;'
+`;
+
 const blog5Body = `
     <div class="accordion-body">
         <h4 class="text-center mb-4">Greetings!</h4>
         <div class='row'>
-            <p class="col-sm-4">Week 5 of <a href="https://xpro.mit.edu/" target="_blank">MIT xPRO</a><span class="text-white"> Full Stack Coding Bootcamp</span> (full-time cohort), we were assigned two projects that would be graded towards our <span class="text-white">Professional Certificate</span>.</p>
-            <p class="col-sm-4"><span class="chocolate menloRegular">PacMan Factory</span> <span class="text-white">Basic Requirements</span>: Create PacMen with 2D movement, images should alternate to simulate 'chomping' and horizontal direction change.</p>
-            <p class="col-sm-4"><span class="chocolate menloRegular">Eye Movement</span> <span class="text-white">Basic Requirements</span>: Use 'onmousemove' event and array methods to simulate an 'pair of eyes' tracking the mouse cursor.</p>
+            <p ${blog5pStyle} class="col-sm-4">Week 5 of <a href="https://xpro.mit.edu/" target="_blank">MIT xPRO</a><span class="text-white"> Full Stack Coding Bootcamp</span> (full-time cohort), we were assigned two projects that would be graded towards our <span class="text-white">Professional Certificate</span>.</p>
+            <p ${blog5pStyle} class="col-sm-4"><span class="chocolate menloRegular">PacMan Factory</span> <span class="text-white">Basic Requirements</span>: Create PacMen with 2D movement, images should alternate to simulate 'chomping' and horizontal direction change.</p>
+            <p ${blog5pStyle} class="col-sm-4"><span class="chocolate menloRegular">Eye Movement</span> <span class="text-white">Basic Requirements</span>: Use 'onmousemove' event and array methods to simulate an 'pair of eyes' tracking the mouse cursor.</p>
         </div>
         <div class="row">
             <div class="col-sm-6">
@@ -17,9 +21,9 @@ const blog5Body = `
             </div>
         </div>
         <div class="row">
-            <p class="col-sm-4"><span class="cornflowerBlue">Customized Aqua-PacMan</span> to be contained within an <span class="text-white">animated aquarium</span>. The move button has been upgraded to a <span class="text-white">color-coded toggle button</span>. You can also <span class="text-white"><em>ZAP!</em></span> a PacMan if you made too many!</p>
-            <p class="col-sm-4"><span class="cornflowerBlue">Customized <span class="text-silver">an</span> Animated Head</span> to '<span class="text-white">blink</span>' and '<span class="text-white">talk</span>' with mouse clicks or mobile touch inputs. Mr. Animated Head is also very <span class="text-white">passionate</span> about his <span class="text-white">bootcamp experience</span>!</p>
-            <p class="col-sm-4">Both projects are <span class="text-white menloRegular">fully responsive</span> and <span class="text-white menloRegular">mobile compatible</span>. Please check out the demos or view the GitHub Repositories from my <a href="../pages/projects.html#animatedHeadCard">Projects</a> page.</p>
+            <p ${blog5pStyle} class="col-sm-4"><span class="cornflowerBlue">Customized Aqua-PacMan</span> to be contained within an <span class="text-white">animated aquarium</span>. The move button has been upgraded to a <span class="text-white">color-coded toggle button</span>. You can also <span class="text-white"><em>ZAP!</em></span> a PacMan if you made too many!</p>
+            <p ${blog5pStyle} class="col-sm-4"><span class="cornflowerBlue">Customized <span class="text-silver">an</span> Animated Head</span> to '<span class="text-white">blink</span>' and '<span class="text-white">talk</span>' with mouse clicks or mobile touch inputs. Mr. Animated Head is also very <span class="text-white">passionate</span> about his <span class="text-white">bootcamp experience</span>!</p>
+            <p ${blog5pStyle} class="col-sm-4">Both projects are <span class="text-white menloRegular">fully responsive</span> and <span class="text-white menloRegular">mobile compatible</span>. Please check out the demos or view the GitHub Repositories from my <a href="../pages/projects.html#animatedHeadCard">Projects</a> page.</p>
         </div>
         <div class="'container-fluid row text-end">
             <div>
