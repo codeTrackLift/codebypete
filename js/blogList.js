e40c53ca39823b26c1a55f726edc56e47d9c457c
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-06-24 18:32:33 -0400

    blog 11 add

---
+++ js/blogList.js
@@ -0,0 +1,115 @@
+let blog11OpenStatus = false;
+let blog10OpenStatus = false;
+let blog9OpenStatus = false;
+let blog8OpenStatus = false;
+let blog7OpenStatus = false;
+let blog6OpenStatus = false;
+let blog5OpenStatus = false;
+let blog4OpenStatus = false;
+let blog3OpenStatus = false;
+let blog2OpenStatus = false;
+let blog1OpenStatus = false;
+let blog0OpenStatus = false;
+
+const resetBlogOpenStatus = () => {
+    blog11OpenStatus = false;
+    blog10OpenStatus = false;
+    blog9OpenStatus = false;
+    blog8OpenStatus = false;
+    blog7OpenStatus = false;
+    blog6OpenStatus = false;
+    blog5OpenStatus = false;
+    blog4OpenStatus = false;
+    blog3OpenStatus = false;
+    blog2OpenStatus = false;
+    blog1OpenStatus = false;
+    blog0OpenStatus = false;
+}
+
+const blogIndex = [
+    {
+        id: 10,
+        number: 'Ten',
+        title: 'Capstone Videos',
+        date: 'Jun 20, 2022'
+    },
+    {
+        id: 9,
+        number: 'Nine',
+        title: 'Frontend Architecture',
+        date: 'May 31, 2022'
+    },
+    {
+        id: 8,
+        number: 'Eight',
+        title: 'Bad Bank Stock Ticker',
+        date: 'May 13, 2022'
+    },
+    {
+        id: 7,
+        number: 'Seven',
+        title: 'To-Do Dashboard',
+        date: 'Apr 23, 2022'
+    },
+    {
+        id: 6,
+        number: 'Six',
+        title: 'Real-Time Bus Tracker',
+        date: 'Mar 22, 2022'
+    },
+    {
+        id: 5,
+        number: 'Five',
+        title: 'PacMan Factory & Eye Movement',
+        date: 'Mar 15, 2022'
+    },
+    {
+        id: 4,
+        number: 'Four',
+        title: 'Custom CSS Typewriter',
+        date: 'Mar 5, 2022'
+    },
+    {
+        id: 3,
+        number: 'Three',
+        title: 'PacMan Mini-Game',
+        date: 'Feb 27, 2022'
+    },
+    {
+        id: 2,
+        number: 'Two',
+        title: 'Magic Ball Challenge',
+        date: 'Feb 20, 2022'
+    },
+    {
+        id: 1,
+        number: 'One',
+        title: 'Creating a GitHub Website',
+        date: 'Feb 16, 2022'
+    },
+    {
+        id: 0,
+        number: 'Zero',
+        title: 'Coding Bootcamp Introduction',
+        date: 'Feb 9, 2022'
+    },
+];
+
+// Open accordion with url hash
+if (window.location.hash) {
+    hash = `${window.location.hash}Open()`;
+    hash = hash.replace('#','').replace('Button','');
+
+    // Remove after adding blog11
+    if (hash === 'blog11Open()') {
+        blogIndex.unshift({
+            id: 11,
+            number: 'Eleven',
+            title: '3 Week Intern Recap',
+            date: 'Jun 25, 2022'
+        })
+    }
+    // Remove after adding blog11
+
+    setTimeout(hash);
+}
--- js/blogs/blog11.js
+++ js/blogs/blog11.js
@@ -1,16 +1,75 @@
+const pStyle = `
+    style='margin: 0.5rem auto;'
+`;
+
+const olStyle = `
+    style = '
+        margin: -1.25rem 0rem -0.25rem 1.25rem;
+    '
+`;
+
 const blog11Body = `
     <div class="accordion-body">
         <div class="d-flex justify-content-evenly my-4">
             <h4 class="my-auto">Greetings!</h4>
         </div>
         <div class="row">
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque iste repellat ea quae sed ipsam vel eius? Id ipsam laboriosam harum ratione perferendis modi maxime, magni mollitia animi eligendi cumque. Iste, deleniti. Reiciendis, doloribus rerum dolor minima, perspiciatis pariatur ullam voluptas ut libero nobis molestias quibusdam voluptatibus sint nihil adipisci.
+            <p ${pStyle}>
+                I never thought I would be so excited to go to work! This is my review of 3 weeks as a 43 year old Software Engineering Intern...
+            </p>
+            <p ${pStyle}>
+                
             </p>
         </div>
         <div class="row">
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque iste repellat ea quae sed ipsam vel eius? Id ipsam laboriosam harum ratione perferendis modi maxime, magni mollitia animi eligendi cumque. Iste, deleniti. Reiciendis, doloribus rerum dolor minima, perspiciatis pariatur ullam voluptas ut libero nobis molestias quibusdam voluptatibus sint nihil adipisci.
+
+            <span>TLDR:</span>
+            <hr>
+            <ol ${olStyle}>
+                <li>Learned how to use Next.js, Sass, Jira, and Confluence</li>
+                <li>Created a ButtonWithImages React component and developed many unit-tests using Jest</li>
+                <li>Participated in team stand-up meetings and investigations</li>
+                <li>Developed and closed 3 Jira Tickets and PRs</li>
+                <li>The Forbes Frontend Engineering team is awesome!</li>
+            </ol>
+            <hr>
+        </div>
+        <div class="row">
+            <p ${pStyle}>
+                It has now been three weeks since I started my full-time paid internship at Forbes Media as part of their Frontend Engineering team. During the second trimester of bootcamp, the career coaches at MIT xPRO hooked me up with a referral for the <a href='https://www.linkedin.com/posts/codebypete_webdevelopment-mernstackdeveloper-activity-6931971649504198656-hdBr?utm_source=linkedin_share&utm_medium=member_desktop_web' target='_blank'>Forbes Engineering Internship</a>.  I wasn't expecting to get called for the interview, but was pleasantly surprised when it happened.  
+            </p>
+            <p ${pStyle}>
+                The interview was conducted over Zoom and the process included a large group introduction, followed by a series of three 1:1 break-out sessions.  For me, each session was with a member of the Frontend Engineering team.  Everybody I spoke to was extraordinarily nice, but they did challenge me with technical JavaScript questions.  In hindsight, there were several areas where I could have articulated a better answer.  However, I was very fortunate, as the Frontend Engineering team decided to give me a chance!
+            </p>
+            <p ${pStyle}>
+                It all became very real when I got my laptop (16" MacBook Pro) and monitor delivered and set up.  Getting local admin on the laptop was the first challenge, this took almost 2 working days, but was well worth the effort!  After getting privileges to execute 'sudo' commands, I started feeling like a developer again!  This feeling did not last long as I immediately slipped back into imposter syndrome when I couldn't get my first 'npm install' to run.  
+            </p>
+            <p ${pStyle}>
+                Without revealing any details, my amazing mentor came to the rescue and helped remedy the issue (missing settings).  From that point on, I was able to use git, clone the appropriate repos, install the necessary packages, and get my local development environment up and running.
+            </p>
+            <p ${pStyle}>
+                The next major milestone was attending and participating in team stand-ups and meetings, and where I learned to use Jira and Confluence.  It is quite the privilege to be able to listen in on these discussions, as they helped fill in missing tech vocabulary.  This also gave me exposure to professional software engineers, their mindset, and how they rationalize real-world issues.
+            </p>
+            <p ${pStyle}>
+                During one of the meetings, it was mentioned that a new component was needed, specifically a button with a nested image.  I got excited, as I had done something similar for my GitHub Readme buttons on the Projects page.  However, I did not have the confidence to bring it up in the meeting.  After wrapping up, I quietly messaged my mentor and showed him a screenshot of my GitHub Readme button.  My mentor and the Frontend Engineering manager discussed, and decided to assign me my first Jira ticket!
+            </p>
+            <p ${pStyle}>
+                Everything I have developed thus far has been coded in a silo.  So working on a large and complex codebase with a group was quite the eye-opener!  Since the internship overlaps with the final trimester of bootcamp, Next.js was completely foreign to me as well as Sass.  These were two technologies I had to pick up right away in order to contribute.  Thankfully, LinkedIn has a course for <a href='https://www.linkedin.com/posts/codebypete_certificate-of-completion-activity-6940040104551878656-7g2o?utm_source=linkedin_share&utm_medium=member_desktop_web' target='_blank'>Server-Side Rendering</a> which covers Next.js and Sass was not too different from regular CSS.
+            </p>
+            <p ${pStyle}>
+                After making the button and verifying that it looks and functions as intended in my local development environment, I had to push it to the GitHub repository and make a pull request.  Imposter syndrome was kicking in hard at this point, as I was about to expose my coding insecurities to the whole world (or so it felt)!  I reached out to a junior developer on the team and shared my files with him, hoping for some reassurance.  He gave me the thumbs up, then encouraged me to push the code, and so I did.
+            </p>
+            <p ${pStyle}>
+                Folks had warned me that your first PR as a developer could result in a lot of comments.  55 comments later, my ButtonWithImages could now be dynamically rendered with different SVGs, text, onClick functionality, and even included a suite of unit tests!  Each comment pointed out weaknesses in my code, like redundant and inefficient code, as well as failure to have good test coverage.
+            </p>
+            <p ${pStyle}>
+                This set a good back and forth tempo with the experienced software engineers on the team.  I could share my attempts at a solution, then see how a master would do it.  By the end of the third week, I had the opportunity to work on 6 React tickets and 3 of them were approved, merged, and closed.  One of the tickets was to investigate an unintended issue; we have a follow-up meeting next week to discuss the investigation findings and determine a path forward.
+            </p>
+            <p ${pStyle}>
+                My first three weeks with Forbes validates my decision to leave my cushy pharmaceutical job to pursue my passion.  I enjoy every minute of the work, learning each new technology feels exciting and rewarding, and the team is warm and welcoming.  Maybe this will change if I ever lose the 'intern' status; however, I will not worry right now as I only have 5 short weeks left with this team.
+            </p>
+            <p ${pStyle}>
+                I hope to make the most out of every minute...
             </p>
         </div>
         <div class="row">
