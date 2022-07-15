c6dca90e0fe62083faf8bd8f09b51f7d38e5e7d4
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-07-15 19:08:43 -0400

    scrolltainer text-white

--- js/selfTaught/scrollTainerCard.js
+++ js/selfTaught/scrollTainerCard.js
@@ -3,8 +3,8 @@ const scrollTainerCard = `
         <img src="../pics/projects/scrollTainerM2CS.jpg" class="card-img-top m-auto img-fluid pt-2" style='width: 10rem;' alt="MIT scrollTainer animated gif with stock ticker.">
         <div class="card-body">
             <h5 class="card-title">ScrollTainer UI Experiment</h5>
-            <p class="card-text">This "mini-project" was developed during my <a href='https://www.linkedin.com/posts/codebypete_webdevelopment-mernstackdeveloper-activity-6931971649504198656-hdBr/?utm_source=linkedin_share&utm_medium=member_desktop_web' target='_blank'>Forbes Media internship</a>.  Built with React and Framer-motion for animations.</p>
-            <p class="card-text">This was one of my contributions to the Forbes UI Showcase.  See <a href="../pages/blog.html#blog11">blog[11]</a>.</p>
+            <p class="card-text">This "<span class='text-white'>mini-project</span>" was developed during my <a href='https://www.linkedin.com/posts/codebypete_webdevelopment-mernstackdeveloper-activity-6931971649504198656-hdBr/?utm_source=linkedin_share&utm_medium=member_desktop_web' target='_blank'>Forbes Media internship</a>.  Built with <span class='text-white'>React</span> and <span class='text-white'>Framer-motion</span> for animations.</p>
+            <p class="card-text">This was one of my contributions to the <span class='text-white'>Forbes UI Showcase</span>.  See <a href="../pages/blog.html#blog11">blog[11]</a>.</p>
             <div class="btnContainer">
                 <a id='scrollTainerDemo' href="https://codetracklift.github.io/scrolltainer/" class="btn btn-secondary" target="_blank">Try Demo</a>
                 <a id='scrollTainerRepo' href="https://github.com/codeTrackLift/scrolltainer#readme" class="btn btn-secondary btnDark" target="'_blank"><img src="../pics/contact/GitHub-Mark-64px.png"> Readme</a>
