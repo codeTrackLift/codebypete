f1c246968d4f9b2744dea854a02325b8207a0fef
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-07-15 19:00:25 -0400

    ScrollTainer

--- js/selfTaught/scrollTainerCard.js
+++ js/selfTaught/scrollTainerCard.js
@@ -2,7 +2,7 @@ const scrollTainerCard = `
     <div id='scrollTainerCard' class="card col-sm-4" style="width: 18rem;">
         <img src="../pics/projects/scrollTainerM2CS.jpg" class="card-img-top m-auto img-fluid pt-2" style='width: 10rem;' alt="MIT scrollTainer animated gif with stock ticker.">
         <div class="card-body">
-            <h5 class="card-title">scrollTainer UI Experiment</h5>
+            <h5 class="card-title">ScrollTainer UI Experiment</h5>
             <p class="card-text">This "mini-project" was developed during my <a href='https://www.linkedin.com/posts/codebypete_webdevelopment-mernstackdeveloper-activity-6931971649504198656-hdBr/?utm_source=linkedin_share&utm_medium=member_desktop_web' target='_blank'>Forbes Media internship</a>.  Built with React and Framer-motion for animations.</p>
             <p class="card-text">This was one of my contributions to the Forbes UI Showcase.  See <a href="../pages/blog.html#blog11">blog[11]</a>.</p>
             <div class="btnContainer">
