acd76d0de3a79d9dbb25b89fe73e26e90951342c
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-07-17 17:08:16 -0400

    add ScrollTainterCard blog13 link

--- js/selfTaught/scrollTainerCard.js
+++ js/selfTaught/scrollTainerCard.js
@@ -4,7 +4,7 @@ const scrollTainerCard = `
         <div class="card-body">
             <h5 class="card-title">ScrollTainer UI Experiment</h5>
             <p class="card-text">This "<span class='text-white'>mini-project</span>" was developed during my <a href='https://www.linkedin.com/posts/codebypete_webdevelopment-mernstackdeveloper-activity-6931971649504198656-hdBr/' target='_blank'>Forbes Media internship</a>.  Built with <span class='text-white'>React</span> and <span class='text-white'>Framer-motion</span> for animations.</p>
-            <p class="card-text">This was one of my contributions to the <span class='text-white'>Forbes UI Showcase</span>.  See <a href="../pages/blog.html#blog11">blog[11]</a>.</p>
+            <p class="card-text">This was one of my contributions to the <span class='text-white'>Forbes UI Showcase</span>.  See <a href="../pages/blog.html#blog13Button">blog[13]</a>.</p>
             <div class="btnContainer">
                 <a id='scrollTainerDemo' href="https://codetracklift.github.io/scrolltainer/" class="btn btn-secondary" target="_blank">Try Demo</a>
                 <a id='scrollTainerRepo' href="https://github.com/codeTrackLift/scrolltainer#readme" class="btn btn-secondary btnDark" target="'_blank"><img src="../pics/contact/GitHub-Mark-64px.png"> Readme</a>
