2e837fa42c7103d2c4c10eb94a625204cb155397
Peter Chu <peterc0523@gmail.com>
2022-10-25 15:15:47 -0400

    update cbp2 card

--- js/selfTaught/cBP2Card.js
+++ js/selfTaught/cBP2Card.js
@@ -5,9 +5,8 @@ const cBP2Card = `
             <h5 class="card-title">codeByPete v2.0</h5>
             <p class="card-text">This is a <span class='text-white'>hand-crafted</span> React app featuring a light and dark mode, and a number of custom assets.</p>
             <p class="card-text">Framer-motion was used for animations.  This site was <span class='text-white'>performance optimized</span> using <a href='https://gtmetrix.com' target='_blank'>GTmetrix</a>.</p>
-            <div class="btnContainer">
+            <div class="btnContainer mx-auto">
                 <a id='cBP2Demo' href="https://codetracklift.github.io/reactbypete/" class="btn btn-secondary" target="_blank">Visit Site</a>
-                <a id='cBP2Repo' href="https://github.com/codeTrackLift/reactbypete#readme" class="btn btn-secondary btnDark" target="'_blank"><img src="../pics/contact/GitHub-Mark-64px.png"> Readme</a>
             </div>
         </div>
         <cite id='cBP2Cite' class="text-center">Developed Aug-Sep, 2022</cite>
@@ -35,7 +34,7 @@ const injectCBP2Card = (section) => {
             
         const cBP2CiteController = new ScrollMagic.Controller();
         const cBP2Cite = new ScrollMagic.Scene({
-                triggerElement: '#cBP2Repo',
+                triggerElement: '#cBP2Demo',
                 offset: -200
             })
             .setClassToggle('#cBP2Cite', 'hide')
